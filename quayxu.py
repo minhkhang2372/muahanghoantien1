12efdc7368f1d53b67b840b47ecad811fa53f321
minhkhang2372 <156471715+minhkhang2372@users.noreply.github.com>
2024-04-21 03:16:26 +0700

    Update quayxu.py

--- quayxu.py
+++ quayxu.py
@@ -6,7 +6,7 @@ from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton
 from telegram.ext import Application, CommandHandler
 
 # Token của bot Telegram
-TOKEN = "QUAY_XU_BOT"
+TOKEN = "${{ secrets.QUAY_XU_BOT }}"
 
 # Định nghĩa hàm lấy dữ liệu từ API Shopee
 def get_api_data(current_time_millis):
